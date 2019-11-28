0559f2a64281132d55b0edbe4f0b484229d53fcc
shoma07 <23730734+shoma07@users.noreply.github.com>
2019-11-28 23:12:08 +0900

    Implement show handler

--- handlers.go
+++ handlers.go
@@ -29,7 +29,25 @@ func PostsIndex(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 func PostsShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "PostsShow")
+	db, err := gorm.Open("sqlite3", "db/development.sqlite3")
+	if err != nil {
+		log.Fatalln("Failed to connect to database")
+	}
+	defer db.Close()
+
+	post := &Post{}
+
+	if db.First(&post, ps.ByName("id")).RecordNotFound() {
+		NotFoundHandler(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", jsonapi.MediaType)
+	w.WriteHeader(http.StatusOK)
+
+	if err := jsonapi.MarshalPayload(w, post); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func PostsCreate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -62,4 +80,17 @@ func PostsUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 func PostsDestroy(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "PostsDestroy")
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", jsonapi.MediaType)
+	w.WriteHeader(http.StatusNotFound)
+
+	if err := jsonapi.MarshalErrors(w, []*jsonapi.ErrorObject{{
+		Title:  "Not Resource Found",
+		Detail: "Not Resource Found",
+		Code:   "404",
+	}}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
